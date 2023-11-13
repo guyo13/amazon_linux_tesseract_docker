c46f4c0d39a8ab3225666de1af9b0aba3b2e66e1
Guy Or <guydude1@gmail.com>
2023-11-13 18:30:41 +0200

    Fix pkg-config issues

--- x86/compile-tess.sh
+++ x86/compile-tess.sh
@@ -5,12 +5,13 @@ wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonic
 	&& mkdir build \
 	&& cd build && cmake .. && make \
     && make install \
-    && cd .. \
+    && cd ../.. \
 	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
 	&& tar -xzf 5.3.3.tar.gz \
 	&& cd tesseract-5.3.3 \
 	&& mkdir build \
 	&& cd build && cmake .. && make \
     && make install \
-    && cd .. \
-    && rm -rf tesseract-5.3.3 leptonica-1.83.1
+    && cd ../.. \
+    && rm -rf tesseract-5.3.3 leptonica-1.83.1 \
+    && mv /usr/local/lib/pkgconfig/* /usr/share/pkgconfig/
