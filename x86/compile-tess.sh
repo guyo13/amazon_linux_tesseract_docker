13d28c030ea50860ee093d712d81b6d4d17e8173
Guy Or <guydude1@gmail.com>
2023-11-13 17:50:45 +0200

    Initial commit

---
+++ x86/compile-tess.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
+	&& tar -xzf leptonica-1.83.1.tar.gz \
+	&& cd leptonica-1.83.1 \
+	&& mkdir build \
+	&& cd build && cmake .. && make \
+    && make install \
+    && cd .. \
+	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
+	&& tar -xzf 5.3.3.tar.gz \
+	&& cd tesseract-5.3.3 \
+	&& mkdir build \
+	&& cd build && cmake .. && make \
+    && make install \
+    && cd .. \
+    && rm -rf tesseract-5.3.3 leptonica-1.83.1
