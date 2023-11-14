35b8fc149739e1c550584dfebdf0a52095a4bad1
Guy Or <guydude1@gmail.com>
2023-11-14 06:53:31 +0200

    fix script

--- x86/compile-tess.sh
+++ x86/compile-tess.sh
@@ -15,6 +15,6 @@ wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonic
 	&& cd build \
     && cmake .. -DBUILD_SHARED_LIBS=ON \
     && make \
-    && make install
+    && make install \
     && cd ../.. \
     && rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz \
