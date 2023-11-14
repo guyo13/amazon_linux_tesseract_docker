a9e9b7061ce1386bb45e6e30043306af9cff3c93
Guy Or <guydude1@gmail.com>
2023-11-14 06:44:21 +0200

    fix compile parameters and env settings

--- x86/compile-tess.sh
+++ x86/compile-tess.sh
@@ -3,15 +3,18 @@ wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonic
 	&& tar -xzf leptonica-1.83.1.tar.gz \
 	&& cd leptonica-1.83.1 \
 	&& mkdir build \
-	&& cd build && cmake .. && make \
+	&& cd build \
+    && cmake .. -DBUILD_SHARED_LIBS=ON \
+    && make \
     && make install \
     && cd ../.. \
 	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
 	&& tar -xzf 5.3.3.tar.gz \
 	&& cd tesseract-5.3.3 \
 	&& mkdir build \
-	&& cd build && cmake .. && make \
-    && make install \
+	&& cd build \
+    && cmake .. -DBUILD_SHARED_LIBS=ON \
+    && make \
+    && make install
     && cd ../.. \
-    && rm -rf tesseract-5.3.3 leptonica-1.83.1 \
-    && mv /usr/local/lib/pkgconfig/* /usr/share/pkgconfig/
+    && rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz \
