fc63074b3e00cb55ed91fdfa83c1baa27b2ab9b7
Guy Or <guydude1@gmail.com>
2023-11-20 05:49:32 +0200

    fix build

--- x86/compile-tess.sh
+++ x86/compile-tess.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
+echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf \
+	&& wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
 	&& tar -xzf leptonica-1.83.1.tar.gz \
 	&& cd leptonica-1.83.1 \
 	&& mkdir build \
 	&& cd build \
-    && cmake .. -DBUILD_SHARED_LIBS=ON \
-    && make \
+   	&& cmake .. -DBUILD_SHARED_LIBS=ON \
+	&& make \
     && make install \
     && cd ../.. \
 	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
@@ -18,3 +19,4 @@ wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonic
     && make install \
     && cd ../.. \
     && rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz \
+	&& ldconfig -v
