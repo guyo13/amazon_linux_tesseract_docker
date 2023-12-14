19821b0cd512aef48be192037832d83c1a50b5ff
Guy Or <guydude1@gmail.com>
2023-12-14 17:26:02 +0200

    wip build both static and shared libs

---
+++ build_amd.sh
@@ -0,0 +1,3 @@
+#!/bin.sh
+
+docker build -t guyor/amazon_linux_tesseract:latest -t guyor/amazon_linux_tesseract:tess_5.3.3_aml_2023 --progress=plain -f amd64/Dockerfile . 2>&1
--- compile-tess.sh
+++ compile-tess.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
+
+do_cmake_shared() {
+   	cmake .. -DBUILD_SHARED_LIBS=ON \
+	&& make \
+    && make install
+}
+
+do_cmake() {
+   	cmake .. \
+	&& make \
+    && make install
+}
+
 echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf \
 	&& wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
 	&& tar -xzf leptonica-1.83.1.tar.gz \
 	&& cd leptonica-1.83.1 \
 	&& mkdir build \
 	&& cd build \
-   	&& cmake .. -DBUILD_SHARED_LIBS=ON \
-	&& make \
-    && make install \
+   	&& do_cmake_shared \
+    && rm -rf * \
+    && do_cmake \
     && cd ../.. \
 	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
 	&& tar -xzf 5.3.3.tar.gz \
 	&& cd tesseract-5.3.3 \
 	&& mkdir build \
 	&& cd build \
-    && cmake .. -DBUILD_SHARED_LIBS=ON \
-    && make \
-    && make install \
+   	&& do_cmake_shared \
+    && rm -rf * \
+    && do_cmake \
     && cd ../.. \
     && rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz \
 	&& ldconfig -v
