92693bd0b20a7a8d18be2f64b2fbabd08eb882d1
Guy Or <guydude1@gmail.com>
2023-12-14 17:42:02 +0200

    refactor to functions

--- compile-tess.sh
+++ compile-tess.sh
@@ -12,24 +12,44 @@ do_cmake() {
     && make install
 }
 
-echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf \
-	&& wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
+setup_ldconfig() {
+    echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf
+}
+
+compile_leptonica() {
+	wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
 	&& tar -xzf leptonica-1.83.1.tar.gz \
 	&& cd leptonica-1.83.1 \
 	&& mkdir build \
 	&& cd build \
    	&& do_cmake_shared \
     && rm -rf * \
-    && do_cmake \
-    && cd ../.. \
-	&& wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
+    && do_cmake
+}
+
+compile_tesseract() {
+    wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
 	&& tar -xzf 5.3.3.tar.gz \
 	&& cd tesseract-5.3.3 \
 	&& mkdir build \
 	&& cd build \
    	&& do_cmake_shared \
     && rm -rf * \
-    && do_cmake \
+    && do_cmake
+}
+
+cleanup() {
+    rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz
+}
+
+main() {
+    setup_ldconfig \
+    && compile_leptonica \
     && cd ../.. \
-    && rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz \
+    && compile_tesseract \
+    && cd ../.. \
+    && cleanup \
 	&& ldconfig -v
+}
+
+main
