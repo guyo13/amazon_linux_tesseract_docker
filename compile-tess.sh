1290e807260f831e554461eef663b28d1ead3f71
Guy Or <guydude1@gmail.com>
2023-12-14 18:33:21 +0200

    change build script

--- compile-tess.sh
+++ compile-tess.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+BASE_DIR=`pwd`
+TESS_DIR="$BASE_DIR/tesseract-5.3.3"
+LEPT_DIR="$BASE_DIR/leptonica-1.83.1"
+
 do_cmake_shared() {
    	cmake .. -DBUILD_SHARED_LIBS=ON \
 	&& make \
@@ -16,38 +20,66 @@ setup_ldconfig() {
     echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf
 }
 
-compile_leptonica() {
+download_leptonica() {
 	wget https://github.com/DanBloomberg/leptonica/releases/download/1.83.1/leptonica-1.83.1.tar.gz \
-	&& tar -xzf leptonica-1.83.1.tar.gz \
-	&& cd leptonica-1.83.1 \
-	&& mkdir build \
-	&& cd build \
-   	&& do_cmake_shared \
-    && rm -rf * \
-    && do_cmake
+	&& tar -xzf leptonica-1.83.1.tar.gz
 }
 
-compile_tesseract() {
+download tesseract() {
     wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.3.3.tar.gz \
-	&& tar -xzf 5.3.3.tar.gz \
-	&& cd tesseract-5.3.3 \
+	&& tar -xzf 5.3.3.tar.gz
+}
+
+compile_shared() {
+	cd $LEPT_DIR\
 	&& mkdir build \
 	&& cd build \
    	&& do_cmake_shared \
     && rm -rf * \
-    && do_cmake
+	cd $TESS_DIR \
+	&& mkdir build \
+	&& cd build \
+   	&& do_cmake_shared \
+    && rm -rf * \
+    && cd $BASE_DIR
+}
+
+compile_static() {
+	cd $LEPT_DIR\
+	&& mkdir build \
+	&& cd build \
+   	&& do_cmake \
+    && rm -rf * \
+	cd $TESS_DIR \
+	&& mkdir build \
+	&& cd build \
+   	&& do_cmake \
+    && rm -rf * \
+    && cd $BASE_DIR
 }
 
 cleanup() {
-    rm -rf tesseract-5.3.3 leptonica-1.83.1 leptonica-1.83.1.tar.gz 5.3.3.tar.gz
+    if [ -d "$TESS_DIR" ]; then
+        rm -rf $TESS_DIR
+    fi
+    if [ -d "$LEPT_DIR" ]; then
+        rm -rf $LEPT_DIR
+    fi
+    if [ -f "leptonica-1.83.1.tar.gz" ]; then
+        rm leptonica-1.83.1.tar.gz
+    fi
+    if [ -f "5.3.3.tar.gz" ]; then
+        rm 5.3.3.tar.gz
+    fi
 }
 
 main() {
-    setup_ldconfig \
-    && compile_leptonica \
-    && cd ../.. \
-    && compile_tesseract \
-    && cd ../.. \
+    cleanup \
+    && setup_ldconfig \
+    && download_leptonica \
+    && download_tesseract \
+    && compile_shared \
+    && compile_static \
     && cleanup \
 	&& ldconfig -v
 }
