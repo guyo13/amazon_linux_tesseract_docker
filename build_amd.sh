8b6ce215027e3e25e85c58188a832e4aca8251ae
Guy Or <guydude1@gmail.com>
2023-12-22 08:06:30 +0200

    add ECR publish tags

--- build_amd.sh
+++ build_amd.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker build -t guyor/amazon_linux_tesseract:latest -t guyor/amazon_linux_tesseract:tess_5.3.3_aml_2023 --progress=plain -f amd64/Dockerfile . 2>&1
+docker build \
+-t public.ecr.aws/x9a3w9a8/amazon_linux_tesseract:latest \
+-t public.ecr.aws/x9a3w9a8/amazon_linux_tesseract:tess_5.3.3_aml_2023 \
+-t guyor/amazon_linux_tesseract:latest \
+-t guyor/amazon_linux_tesseract:tess_5.3.3_aml_2023 \
+--progress=plain -f amd64/Dockerfile . 2>&1
